be1f7a1f8e4b9228cbc5dfddb27a2e7bd9d0ad7b
Lakshith Nagarur Lakshminarayana Reddy <lakshith.nagarur.lakshminarayana.reddy@audi.de>
2025-01-30 17:33:44 +0100

    autoplay and button

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const daysEl = document.getElementById("days");
 const hoursEl = document.getElementById("hours");
 const minutesEl = document.getElementById("minutes");
 const secondsEl = document.getElementById("seconds");
+const audio = document.getElementById("background-music");
+
+// Play Music Automatically
+audio.play();
 
 function updateCountdown() {
     const now = new Date();
